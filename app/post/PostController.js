dec2fef71b6f5c34d6d7518101d742ce44356eae
giri <g.sivadanam@patagona.de>
2016-12-20 16:00:46 +0530

    fixed id missing but

--- app/post/PostController.js
+++ app/post/PostController.js
@@ -3,6 +3,7 @@
  */
 angular.module('cms')
   .controller('PostController',["$scope","$stateParams","PostsService",function($scope,$stateParams,PostsService){
+    $scope.id = $stateParams.id
     PostsService.getPostById($stateParams.id).then(function(response){
       $scope.post = response
     })
