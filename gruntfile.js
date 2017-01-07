def70903fa5496661bb0e9376334459dc8d7b995
giri <g.sivadanam@patagona.de>
2017-01-07 23:55:15 +0530

    fixed file concatination bug

--- gruntfile.js
+++ gruntfile.js
@@ -11,18 +11,10 @@ module.exports = function (grunt) {
         dest: 'app/built.js'
       },
       li: {
-        src: ["app/lib/jquery/dist/jquery.min.js",
-          "app/lib/angular/angular.min.js",
-          "app/lib/angular-animate/angular-animate.min.js",
-          "app/lib/angular-aria/angular-aria.min.js",
+        src: [
           "app/lib/angular-messages/angular-messages.min.js",
-          "app/lib/angular-route/angular-route.min.js",
-          "app/lib/angular-material/angular-material.min.js",
-          "app/lib/angular-ui-router/release/angular-ui-router.min.js",
           "app/lib/angular-bootstrap/ui-bootstrap.min.js",
           "app/lib/angular-md5/angular-md5.min.js",
-          "app/lib/angular-moment/angular-moment.min.js",
-          "app/lib/moment/min/moment.min.js",
           "app/lib/ng-file-upload/ng-file-upload.min.js",
           "app/lib/tinymce/tinymce.min.js",
           "app/lib/angular-ui-tinymce/dist/tinymce.min.js"],
